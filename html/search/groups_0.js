6a998a3cc59966944930dc8cff8047c776276a58
maurodangelo <maurodangelo@users.noreply.github.com>
2024-04-21 10:22:41 +0200

    reworked doxygen documentation

--- html/search/groups_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['page_20pg2_5femb_5fadv_0',['page pg2_emb_adv',['../group__bitfields.html',1,'']]],
-  ['pg2_5femb_5fadv_1',['page pg2_emb_adv',['../group__bitfields.html',1,'']]]
-];
